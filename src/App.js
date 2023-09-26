436c7e09f12dc04672e61e01a1acafc4fc33f3db
Yulia Tarima <yuliatarima@gmail.com>
2023-09-26 12:43:36 -0400

    Refactor: Single function handles increment/decrement counter

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import {countersInitialArr} from "./AppData.js";
 import './App.css';
 //import Counter from "./Counter";
 
-const calcTotal = (objArr) => {
-    return objArr.reduce((acc, currEl) => acc + currEl.value, 0);
-}
+const calcTotal = (objArr) => objArr.reduce((acc, currEl) => acc + currEl.value, 0);
 
 function App() {
     const [totalCount, setTotalCount] = useState(calcTotal(countersInitialArr));
@@ -15,10 +13,7 @@ function App() {
 
     function updateCountersArrStateOnValueChange(counterObj, countersArrCurrState) {
 
-        /**
-         * update current counters array that is stored in state
-         * with a changed value of an updated counter
-         */
+        /** update current counters array that is stored in state with a changed value of an updated counter */
         /*const updatedCountersArr = [];
         for (let i = 0; i < countersArrCurrState.length; i++) {
             if (countersArrCurrState[i].id === counterObj.id) {
@@ -31,30 +26,25 @@ function App() {
         }*/
         const updatedCountersArr = countersArrCurrState.map(el =>
             el.id === counterObj.id
-                ? {
-                    id: el.id,
-                    value: counterObj.value
-                }
+                ? {id: el.id, value: counterObj.value}
                 : el
         );
 
-        /**
-         * Set new total in state
-         * from value sum calculations of an updated above counters array
-         */
+        /** Set new total in state from value sum calculations of an updated above counters array */
         setTotalCount(calcTotal(updatedCountersArr));
 
-        /**
-         * Store updated counters array in state
-         */
+        /** Store updated counters array in state */
         setCountersDynamicState(updatedCountersArr);
     }
 
-
+    const counterActionHandler = (counterObj, counterAction, countState, setCountSetter) => {
+        const newCountState = counterAction === 'plus' ? ++countState : --countState;
+        setCountSetter(newCountState);
+        updateCountersArrStateOnValueChange({id: counterObj.id, value: newCountState}, countersDynamicState);
+    }
     const countChangePlusHandler = (counterObj, countState, setCountSetter) => {
         const newCountState = ++countState;
         setCountSetter(newCountState);
-        console.log('newCountState local counter', newCountState);
         updateCountersArrStateOnValueChange({id: counterObj.id, value: newCountState}, countersDynamicState);
     }
     const countChangeMinusHandler = (counterObj, countState, setCountSetter) => {
@@ -71,16 +61,20 @@ function App() {
             <hr/>
 
             <div>
-                <button onClick={() => countChangeMinusHandler(countersInitialArr[0], count1, setCount1)}>-</button>
+                <button onClick={() => counterActionHandler(countersInitialArr[0], 'minus', count1, setCount1)}>-</button>
+                {/*<button onClick={() => countChangeMinusHandler(countersInitialArr[0], count1, setCount1)}>-</button>*/}
                 {count1}
-                <button onClick={() => countChangePlusHandler(countersInitialArr[0], count1, setCount1)}>+</button>
+                {/*<button onClick={() => countChangePlusHandler(countersInitialArr[0], count1, setCount1)}>+</button>*/}
+                <button onClick={() => counterActionHandler(countersInitialArr[0], 'plus', count1, setCount1)}>+</button>
             </div>
             <hr/>
 
             <div>
-                <button onClick={() => countChangeMinusHandler(countersInitialArr[1], count2, setCount2)}>-</button>
+                <button onClick={() => counterActionHandler(countersInitialArr[1], 'minus', count2, setCount2)}>-</button>
+                {/* <button onClick={() => countChangeMinusHandler(countersInitialArr[1], count2, setCount2)}>-</button>*/}
                 {count2}
-                <button onClick={() => countChangePlusHandler(countersInitialArr[1], count2, setCount2)}>+</button>
+                {/*<button onClick={() => countChangePlusHandler(countersInitialArr[1], count2, setCount2)}>+</button>*/}
+                <button onClick={() => counterActionHandler(countersInitialArr[1], 'plus', count2, setCount2)}>+</button>
             </div>
 
             {/*
